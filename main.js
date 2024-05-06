ba581acf118e1d903f0fea9753a9039d775af77c
Mohammed <gsft22221@gmail.com>
2024-05-06 21:33:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
-var button = document.getElementsByClassName('generate-btn')[0];
+var button = document.getElementsByClassName('generate-btn')[0]
 
 const randomHexColorCode = () => {
-  let n = (Math.random() * 0xfffff * 1000000).toString(16);
-  return '#' + n.slice(0, 6);
-};
-
-const setBgColor = (hex) => {
-  if (typeof hex === 'string') {
-    document.body.style.backgroundColor = hex;
-  }
+	let n = (Math.random() * 0xfffff * 1000000).toString(16)
+	return '#' + n.slice(0, 6)
 }
 
-button.addEventListener('click', () => {
-  let color = randomHexColorCode();
-  setBgColor(color);
+const handleColorGeneration = () => {
+	let color = randomHexColorCode()
 
-  navigator.clipboard.writeText(color)
-})
+	document.body.style.backgroundColor = color
+
+	navigator.clipboard.writeText(color)
+}
+
+button.addEventListener('click', handleColorGeneration)
